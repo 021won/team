e1a132ea98b20144e1318f67b4d6d139a554baf6
DESKTOP-PTUNS30\tj-bu-702-09 <admin>
2024-01-11 14:29:32 +0900

    프로젝트 초기화

---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./css/reset.css">
+  <!-- 작업완료 후 개인적으로 변경할 것 -->
+  <script src="http://code.jquery.com/jquery-latest.js"></script>
+  <script src="./js/common.js"></script>
+  <title>Document</title>
+</head>
+<body>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php include "header.php" ?>
+
+<?php include "section1.php" ?>
+<?php include "section2.php" ?>
+
+<?php include "footer.php" ?>
